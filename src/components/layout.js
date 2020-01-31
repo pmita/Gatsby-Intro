545dd4c49264658299deb2b901d50a324c58872f
Mita <pmita@dxc.com>
2020-01-31 11:34:11 +0000

    first commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,42 +7,19 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+//import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import "./bootstrap.min.css"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+/*We are defining the main layout of our website on top of which (in fact it's more like within) each of our
+components will be positioned. For now we are just initiating the Layout and simply return all the children 
+elements (future components) as a fragement. This is still react and we need to return something.*/
+const Layout = ({ children }) => (
+  <>
+    {children}
+  </>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
